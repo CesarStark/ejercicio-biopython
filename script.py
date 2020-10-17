406b61934c1a2ae9b4589a12d33f1f7d0073cc26
Cesar <72842555+CesarStark@users.noreply.github.com>
2020-10-16 21:36:27 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,23 @@
-from Bio.Seq import Seq
-from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
+
+# Ruta donde se encuentra el archivo en formato GenBank del escritorio (Hepacivirus C)
+filename = "/mnt/c/Users/cesar/Desktop/bioinformatica/sequence.gb" 
+
 def summarize_contents(filename):
+	listA = []
+	listA = os.path.split(filename)
+	print("file:", listA[1], "\npath:", listA[0])
 	all_records = []
-	record = list(SeqIO.parse(filename,"genbank"))
-	print("Path: ",os.path.dirname(filename))
-	print("Num_record = %i records" %len(record))
-	print("\n")
-	for seq_r in SeqIO.parse(filename,"genbank"):
-		all_records.append(seq_r.name)
-		print("Name: ",seq_r.name)
-		print("ID :",seq_r.id)
-		print("Location:")
-		for seq_features in seq_r.features :
-			print('Star: %d, Stop: %d' %(int(seq_feature.location.start),int(seq_feature.location.end)))
-sunnarize_contents(filename)
+	records = list(SeqIO.parse(filename, "genbank"))
+	print("num_records = %i records" % len(records))
+	print("records:")
+	for seq_record in SeqIO.parse(filename, "genbank"):
+		all_records.append(seq_record.name)
+		print("- id:",seq_record.id)
+		print("name: ", seq_record.name)
+		print("description: ", seq_record.description)
+		print("\n")
+		
+		summarize_contents(filename)
