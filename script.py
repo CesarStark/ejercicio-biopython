fe9338c7c8aa3228b71d93cc8c7f2900834e4932
Cesar <72842555+CesarStark@users.noreply.github.com>
2020-10-27 15:20:28 -0700

    Update script.py

--- script.py
+++ script.py
@@ -2,22 +2,33 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import os
 
-# Ruta donde se encuentra el archivo en formato GenBank del escritorio (Hepacivirus C)
-filename = "/mnt/c/Users/cesar/Desktop/bioinformatica/sequence.gb" 
+filename = os.path.abspath("data/ls_orchid.gbk")
 
+#Creacion de funcion
 def summarize_contents(filename):
-	listA = []
-	listA = os.path.split(filename)
-	print("file:", listA[1], "\npath:", listA[0])
-	all_records = []
-	records = list(SeqIO.parse(filename, "genbank"))
-	print("num_records = %i records" % len(records))
-	print("records:")
-	for seq_record in SeqIO.parse(filename, "genbank"):
-		all_records.append(seq_record.name)
-		print("- id:",seq_record.id)
-		print("name: ", seq_record.name)
-		print("description: ", seq_record.description)
-		print("\n")
-		
-		summarize_contents(filename)
+	listaOs = os.path.split(filename)
+	listaExt = os.path.splitext(filename)
+	if (listaExt[1] == ".gbk"):
+		type_file= "genbank"
+	else: 
+		type_file= "fasta"
+	record = list(SeqIO.parse(filename, type_file))
+	#Creacion de diccionario
+	d = {}
+	d['File:'] = listaOs[1]
+	d['Path:'] = listaOs[0]
+	d['Num_records:'] = len(record)
+	#Diccionario con listas
+	d['Names:'] = []
+	d['IDs:'] = []
+	d['Descriptions'] = []
+	#Registro de records
+	for seq_rcd in SeqIO.parse(filename,type_file):
+		d['Names:'].append(seq_rcd.name)
+		d['IDs:'].append(seq_rcd.id)
+		d['Descriptions'].append(seq_rcd.description)
+	return d
+#Imprimir la funcion
+if _name_ == "_main_":
+	resultados = summarize_contents(filename)
+	print(resultados)
